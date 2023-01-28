6cf1bd29cf3b26987bd90c08ad8a526bd9c19701
Hashir <hashirsami@hotmail.ca>
2023-01-28 16:06:43 -0500

    added maps api
    donuts yum

---
+++ static/data.js
@@ -0,0 +1,11 @@
+eqfeed_callback(
+    {"type":"FeatureCollection","features":[
+        {
+            "type":"Feature",
+            "geometry":{"type":"Point","coordinates":[-76.492745,44.2313258,0]},
+            "name":"Tim Horton\'s",
+            'formatted_address': '284 Earl St, Kingston, ON K7L 3N6, Canada',
+            "food":"Donuts",
+            "img":"/donuts.png"
+        },
+    ]});
