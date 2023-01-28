dbff84b880ab59ff8b2b6c724962ed5af4a71e5d
Hashir <hashirsami@hotmail.ca>
2023-01-28 16:22:57 -0500

    input form

--- static/data.js
+++ static/data.js
@@ -3,8 +3,8 @@ eqfeed_callback(
         {
             "type":"Feature",
             "geometry":{"type":"Point","coordinates":[-76.492745,44.2313258,0]},
-            "name":"Tim Horton\'s",
-            'formatted_address': '284 Earl St, Kingston, ON K7L 3N6, Canada',
+            "name":"Starbucks",
+            'formatted_address': '121 Division St, Kingston, ON K7L 3M5, Canada',
             "food":"Donuts",
             "img":"/donuts.png"
         },
